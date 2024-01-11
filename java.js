cbe164ffd910af2e4a7bc2192a3bea93d2d95f29
MaximoAlejandroKirichuk <MaximoAlejandro.Kirichuk@alumnos.uai.edu.ar>
2024-01-11 15:32:41 -0300

    firts day

---
+++ java.js
@@ -0,0 +1,9 @@
+
+document.getElementById('open-menu').addEventListener('click', function() {
+    var menu = document.getElementById('menu');
+    if (menu.style.display === "none") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+});
