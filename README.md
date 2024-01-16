0ed627af3a5f50c06cc6bc957d10fae2dffc0ea4
MaximoAlejandroKirichuk <MaximoAlejandro.Kirichuk@alumnos.uai.edu.ar>
2024-01-16 11:08:03 -0300

    first commit

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 # CampichueloMuebles
 # CampichueloMuebles
 # CampichueloMuebles
+# CampichueloMuebles
--- java.js
+++ java.js
@@ -1,9 +1,11 @@
-
-document.getElementById('open-menu').addEventListener('click', function() {
-    var menu = document.getElementById('menu');
-    if (menu.style.display === "none") {
-        menu.style.display = "block";
-    } else {
-        menu.style.display = "none";
-    }
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('open-menu').addEventListener('click', function() {
+        let menu = document.getElementById('menu');
+        if (menu.style.display === "none") {
+            menu.style.display = "block";
+        } else {
+            menu.style.display = "none";
+        }
+    });
 });
+
