e169a28a4fd08cd7a1aaac95b89aab159714e9c6
MaximoAlejandroKirichuk <MaximoAlejandro.Kirichuk@alumnos.uai.edu.ar>
2024-03-29 16:07:00 -0300

    pruebas 4

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
-// Importa express y otras dependencias necesarias
 const express = require('express');
 const path = require('path');
-
-// Crea una instancia de express
 const app = express();
 
-// Configura el puerto
 const port = process.env.PORT || 3000;
 
-// Configura el motor de vistas y la carpeta de vistas
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-// Sirve archivos estáticos desde la carpeta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Define tus rutas y lógica de la aplicación aquí
 
-// Inicia el servidor
 app.listen(port, () => {
     console.log(`Servidor iniciado en http://localhost:${port}`);
 });
